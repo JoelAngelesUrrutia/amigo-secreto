e3ed32487e6abb1f651696d6aa5a5fb2d8b39ef1
Joel Angeles <angelesurrutia.j@gmail.com>
2025-01-20 16:05:18 -0600

    add sortearAmigo function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const listaAmigos = document.getElementById('listaAmigos');
+
 function agregarAmigo() {
     const amigo = document.getElementById('amigo');
 
@@ -6,12 +8,27 @@ function agregarAmigo() {
         return;
     }
 
-    const listaAmigos = document.getElementById('listaAmigos');
-
     const nuevoAmigo = document.createElement('li');
     nuevoAmigo.textContent = amigo.value;
 
     listaAmigos.appendChild(nuevoAmigo);
 
     amigo.value = "";
+}
+
+function sortearAmigo(){
+    const amigos = listaAmigos.getElementsByTagName('li');
+
+    if (amigos.length === 0) { // Validamos si la lista está vacía
+        alert("La lista está vacía, agrega amigos primero.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length); // Genera un índice aleatorio
+    const amigoAleatorio = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = "El amigo secreto sorteado es: " + amigoAleatorio.textContent
+
+    listaAmigos.textContent = ""
 }
